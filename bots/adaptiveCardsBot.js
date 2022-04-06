ad65551c56a5a1b7414f408481027c2e99b62d64
Shubham Naroji <47168511+narojishubham@users.noreply.github.com>
2022-04-06 15:31:08 +0530

    added code

--- bots/adaptiveCardsBot.js
+++ bots/adaptiveCardsBot.js
@@ -3,6 +3,13 @@
 
 const { ActivityHandler, CardFactory } = require("botbuilder");
 
+// Import AdaptiveCard content.
+// const FlightItineraryCard = require("../resources/FlightItineraryCard.json");
+// const ImageGalleryCard = require("../resources/ImageGalleryCard.json");
+// const LargeWeatherCard = require("../resources/LargeWeatherCard.json");
+// const RestaurantCard = require("../resources/RestaurantCard.json");
+// const SolitaireCard = require("../resources/SolitaireCard.json");
+
 var ACData = require("adaptivecards-templating");
 
 // Import AdaptiveCard content.
